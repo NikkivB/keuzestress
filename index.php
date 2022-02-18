b3175e224c87326f2771c7ad7669db303e87cd1c
Nikki van Braam <0316641@student.rocvantwente.nl>
2022-02-18 19:36:05 +0100

    base form and randomChoice function

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php
+if(isset($_POST['makeChoice'])) {
+    $choices = array(
+        $_POST['choice1'],
+        $_POST['choice2']
+    );
+
+    $finalChoice = $choices[rand(0,1)];
+
+    echo $finalChoice;
+}
